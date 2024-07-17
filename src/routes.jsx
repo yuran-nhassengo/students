233e83f1089c47f33b0c09c353254dea6680f746
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-17 11:29:52 +0200

    Fixe route erro

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import {Home} from './pages/home'
+import {Home} from './pages/Home'
 import { ErrorPage } from "./pages/error-page";
 import { StudentListPage } from "./pages/student-list-page";
 import { ContactForm } from "./pages/contact-form";
